acec7c847daf56d39f61346f05614466f475f618
Joshua Pulung <jeppungdev@gmail.com>
2026-05-02 20:30:41 +0700

    feat(app): adding initial tab ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -43,6 +46,6 @@ export default function App() {
       >
         Claude Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
